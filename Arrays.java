a672a07a43cabf4bcd1c4a167b592b1377d45861
Aman <55349238+X-genius@users.noreply.github.com>
2022-03-05 22:46:47 +0530

    4 ques add

--- Arrays.java
+++ Arrays.java
@@ -201,3 +201,199 @@ class Solution {
         return max_sum > 0 ? max_sum : -1;
     }
 }
+
+// ##################### Sort an array 0's, 1's and 2's ###########################
+
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        int temp;
+        
+        while(mid <= high)
+        {
+            if(a[mid] == 0)
+            {
+              temp = a[mid];
+              a[mid] = a[low];
+              a[low] = temp;
+              low++;
+              mid++;
+            }
+            
+            else if(a[mid] == 1)
+            {
+                mid++;
+            }
+            
+            else if(a[mid] == 2)
+            {
+                temp = a[mid];
+                a[mid] = a[high];
+                a[high] = temp;
+                high--;
+            }
+        }
+        
+        
+        
+        // int cnt0 = 0;
+        // int cnt1 = 0;
+        // int cnt2 = 0;
+        
+        // for(int i = 0; i < n; i++)
+        // {
+        //     if(a[i] == 0)
+        //     {
+        //         cnt0++;
+        //     }
+        //     else if(a[i] == 1)
+        //     {
+        //         cnt1++;
+        //     }
+        //     else if(a[i] == 2)
+        //     {
+        //         cnt2++;
+        //     }
+        // }
+        
+        // for(int i = 0; i < cnt0; i++)
+        // {
+        //     a[i] = 0;
+        // }
+        
+        // for(int i = cnt0; i < cnt0 + cnt1; i++)
+        // {
+        //     a[i] = 1;
+        // }
+        
+        // for(int i = cnt0 + cnt1; i < cnt0 + cnt1 + cnt2; i++)
+        // {
+        //     a[i] = 2;
+        // }
+    }
+    
+};
+
+
+//############################ Move all negative elements to end ###########################################
+
+class Solution {
+    
+    public void segregateElements(int arr[], int n)
+    {
+        ArrayList<Integer> resList = new ArrayList<Integer>();
+        
+        for(int i = 0; i < n; i++)
+        {
+           if(arr[i] >= 0)
+           {
+               resList.add(arr[i]);
+           }
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(arr[i] < 0)
+            {
+                resList.add(arr[i]);
+            }
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = resList.get(i);
+        }
+    }
+}
+
+
+//############################# Move all the negative elements to one side of the array #######################
+
+public class Gfg {
+ 
+    // a utility function to swap two elements of an array
+    public static void swap(int[] ar, int i, int j)
+    {
+        int t = ar[i];
+        ar[i] = ar[j];
+        ar[j] = t;
+    }
+ 
+    // function to shilf all negative integers to the left
+    // and all positive integers to the right
+    // using Dutch National Flag Algorithm
+    public static void move(int[] ar)
+    {
+        int low = 0;
+        int high = ar.length - 1;
+        while (low <= high) {
+            if (ar[low] <= 0)
+                low++;
+            else
+                swap(ar, low, high--);
+        }
+    }
+ 
+    // Driver code
+    public static void main(String[] args)
+    {
+        int[] ar = { 1, 2,  -4, -5, 2, -7, 3,
+                     2, -6, -8, -9, 3, 2,  1 };
+        move(ar);
+        for (int e : ar)
+            System.out.print(e + " ");
+    }
+}
+
+
+//#################### Cyclic rotate by one #############################
+
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+        int last_ele = arr[n - 1];
+        for(int i = n - 1; i > 0; i--)
+        {
+           arr[i] = arr[i - 1];
+        }
+        arr[0] = last_ele;
+    }
+}
+
+// ################### Duplicate in an array with constant space #####################
+
+class Solution {
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+        ArrayList<Integer> resList = new ArrayList<>();
+        ArrayList<Integer> minusOne = new ArrayList<>();
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            int index = arr[i] % n;
+            arr[index] = arr[index] + n;
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            if((arr[i] / n) >= 2)
+            {
+                count++;
+                resList.add(i);
+            }
+        }
+        
+        if(count == 0)
+        {
+            minusOne.add(-1);
+            return minusOne;
+        }
+        
+        return resList;
+    }
+}
