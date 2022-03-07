59bf66293b594386ec0cbf2b43e44a95e261e3f1
Aman <55349238+X-genius@users.noreply.github.com>
2022-03-07 21:31:30 +0530

    add 2 questions

--- Arrays.java
+++ Arrays.java
@@ -397,3 +397,57 @@ class Solution {
         return resList;
     }
 }
+
+//######################## Union of two arrays ################################
+
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        HashSet<Integer> hm = new HashSet<>();
+        
+        for(int i = 0; i < n; i++)
+        {
+            hm.add(a[i]);
+        }
+        
+        for(int j = 0; j < m; j++)
+        {
+            hm.add(b[j]);
+        }
+        
+        return hm.size();
+    }
+}
+
+
+
+//###################### Intersection of two array ################################
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set1 = new HashSet<>();
+        
+        for(int i = 0; i < nums1.length; i++)
+        {
+            set.add(nums1[i]);
+        }
+        
+        for(int i = 0; i < nums2.length; i++)
+        {
+            if(set.contains(nums2[i]))
+            {
+                set1.add(nums2[i]);
+            }
+        }
+        
+        int[] arr = new int[set1.size()];
+        int i = 0;
+        for(Integer k : set1)
+        {
+            arr[i] = k;
+            i++;
+        }
+        return arr;
+    }
+}
